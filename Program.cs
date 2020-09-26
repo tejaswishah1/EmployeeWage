8acf44cdf898d888f9d0d53ea6f1bbe06599cc75
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-26 07:21:11 -0700

    [tejas] add message switch case added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,54 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
+            ////Constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            ////Checking attendance
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+            
+            //variable
+            int empHrs =0;
+            int empWage = 0;
+            
+            //Computation of wages: 
+            empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+
+            else
+                empHrs = 4; ////Assuming part time hours as 4
+            //// switch case to calculate part time wages
+            switch (empCheck)
+            {
+                case 0:
+                    empHrs = 4;
+                    break;
+                case 1:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("Employee wage = " + empWage);
         }
     }
-}
+    }
+
