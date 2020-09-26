9ac827da0a1f35ea8c421d2b9632adef7b27e9f2
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-26 06:45:52 -0700

    [tejas] add message attendance

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Program");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
