1609aca3c0ad41e64dff3b86261c315fc6e8719b
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-26 06:42:42 -0700

    [tejas] add message welcome

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee Wage Program");
         }
     }
 }
