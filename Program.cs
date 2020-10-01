cfc33ef8002e36956b450cb3055875e13731ea3c
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-01 15:23:20 +0530

    [tejas] add Different companies added

--- Program.cs
+++ Program.cs
@@ -1,32 +1,53 @@
 ﻿using System;
 
-namespace Employee_Wages
+namespace EmployeeWage
 {
     class Program
     {
         static void Main(string[] args)
+        { }
+            //// Constants
+        public const int NUMBER_OF_DAYS = 20;
+        public const int IS_FULL_TIME = 8;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int MAX_HRS = 100;
+
+    
+
+        public static void Main()
         {
-
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            //variable
-            int empHrs = 0;
-            int empWage = 0;
+            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            CalculateWage("Reliance", 20, 40, 100);
+            CalculateWage("Bajaj", 30, 50, 96);
+            ////Check Attendance function
             Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-            switch (empCheck)
-            {
-            case 0: empHrs = 4;
-            break;
-            case 1: empHrs = 8;
-            break;
-                default: empHrs = 0;
-                    break; 
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
 
-            Console.WriteLine("Employee wage = " + empWage);
         }
+
+        private static void CalculateWage(string company, int workDays, int empRate, int workHrs)
+        {
+            Random random = new Random();
+            int empCheck = 0, empHrs = 0, totalHrs = 0, empWage = 0, totalEmpWage = 0, days = 0;
+            while (totalHrs <= workHrs && days < workDays)
+            {
+                empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                    totalHrs += empHrs;
+                }
+                else
+                {
+                    empHrs = 4;
+                    totalHrs += empHrs;
+                }
+                days += 1;
+            }
+            empWage = totalHrs * empRate * workDays;
+            totalEmpWage += empWage;
+            Console.WriteLine("Employee wage for " + company + " = " + totalEmpWage);
+
+        } 
     }
-}
+    }
+
